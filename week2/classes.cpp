06d5514815188b5324daec111abbc4d78c1f9700
ivan jaimes <ijaimes2872@gmail.com>
2018-06-12 11:30:03 -0500

    getters/setters/

--- week2/classes.cpp
+++ week2/classes.cpp
@@ -24,15 +24,47 @@ class myObj{
 };
 
 
+class Date{
 
+public:
+    Date(int y, int m, int d){
+        year = y;
+        month = m;
+        day = d;
+    }
+
+    int getYear() { return year;}
+    int getMonth(){ return month;}
+    int getDay()  { return day;}
+
+    void setYear(int yr) {
+        year = yr;
+    }
+    void setDay(int dy) {
+        day = dy;
+    }
+    void setMonth(int mth) {
+        if(mth < 1 || mth > 12){
+            cout << "Not valid" << endl;
+        }
+        month = mth
+    }
+
+    void print(){
+        cout << "The date is " << month << "/" << day << "/" << year << endl;
+    }
+
+// private stuff at the bottom, public top.
+private:
+    int year;
+    int month;
+    int day;
+};
 
 
 int main(){
 
-    myObj var1 {"bob", 30};
-
-
-    var1.print();
-
+    Date date {2018, 6, 12};
+    date.print();
     return 0;
 }
